dd1a4c4889970a31899138406a04864d40db86bc
Cathy Richards <catherine.richards@bayer.com>
2023-05-03 14:19:39 -0500

    Adding padding between fields and utilizing Roboto font

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -75,12 +75,27 @@ const FormComponent = ({
     <div style={{ padding: "1rem" }}>
       <Paper elevation={1} style={{ padding: "0.5rem" }}>
         <Grid container spacing={1} style={{ padding: "0.5rem" }}>
-          {title && <Typography variant="h6">{title}</Typography>}
-          {map(fields, (field) => (
-            <Grid item xs={12} style={{ ...field.containerStyle }}>
-              {createField(field)}
-            </Grid>
-          ))}
+          {title && (
+            <Typography variant="h6" style={{ marginLeft: "0.6rem" }}>
+              {title}
+            </Typography>
+          )}
+          {map(fields, (field, index) => {
+            const bottomPadding =
+              index === fields.length - 1 ? "0rem" : "0.5rem";
+            return (
+              <Grid
+                item
+                xs={12}
+                style={{
+                  paddingBottom: bottomPadding,
+                  ...field.containerStyle,
+                }}
+              >
+                {createField(field)}
+              </Grid>
+            );
+          })}
         </Grid>
       </Paper>
     </div>
